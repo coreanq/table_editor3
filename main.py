9d4cd27f896d061acc21b1e50b9a8233434d9219
CharlesPark <coreanq@gmail.com>
2016-09-19 17:15:41 +0900

    table 그룹 및 파라미터 파싱 루틴 작성중

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import pandas as pd
 # \{(([^\n,{}]+)[,]?)+\},(.)+
 
 TARGET_DIR = r'D:\download\1'
-re_grp = re.compile(r't_ast([A-Z]{3})grp')
-re_params = re.compile(r'\{(([^\n,{}]+)[,]?)+\},(.)+', re.MULTILINE or re.DOTALL)
-
+re_check_grp = re.compile(r't_ast([A-Z]{2:3})grp')
+re_check_params = re.compile(r'\{([^\n,{}]+[,]?)+\}', re.DOTALL)
+re_parse_params = re.compile(r'(\([^,\n{}]+\))?([^\n,{}]+)', re.DOTALL)
 def read_grp(line):
     #re.match 는 문장 처음부터 찾는다. 중간거는 못찾음  
-    matchObj = re_grp.search(line)
+    matchObj = re_check_grp.search(line)
     if( matchObj ) :
         return matchObj.group(1)
     return None 
 
 def read_params(line):
-    matchObj = re_params.search(line)
-    if( matchObj ) :
-        return matchObj.groups()
-    return None
+    # 처음엔 파라미터 형식에 맞는 라인을 찾는다. 
+    lineMatchObj = re_check_params.search(line)
+    params = None
+    params = None
+    if(lineMatchObj):
+        params = re_parse_params.findall(line) 
+        return params
+    else: return None
 
 def test():
     for root, directories, filenames in os.walk(TARGET_DIR):
@@ -26,21 +30,19 @@ def test():
         
         for filename in filenames:
             filePath = root + os.sep + filename
+            grpName = ""
             # print(filePath)
             if(filename == 'KpdPara_Table.c'):
                 with open(filePath, 'r', encoding= 'utf8') as f:
                     for line in f.readlines():
-                        grpName = read_grp(line)
-                        params = read_params(line)
-                        if( params ) :
-                            print(params)
-                     
-
+                        result  = read_grp(line)
+                        if( result ):
+                            grpName = result
+                        params = read_params(line) 
+                        if( params ):
+                            print(grpName, params)
 
                
-            # pass
-            # pass
-    pass 
 if __name__ == '__main__':
     test() 
     print('hello world')
