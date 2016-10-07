e75ea289cad32cf91d55f49408442694e4a79967
CharlesPark <coreanq@gmail.com>
2016-10-07 18:58:14 +0900

    그룹명 클릭하면 해당 그룹 파라미터 내용만 나오도록 수정

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,8 @@
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui    import QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAbstractItemView
+from PyQt5.QtGui  import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QSortFilterProxyModel, QModelIndex, QRegExp 
 import mainwindow_ui 
 import read_data as rd
 
@@ -10,10 +11,28 @@ class MainWindow(QMainWindow, mainwindow_ui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.modelGroup = QStandardItemModel(self)
-        self.modelGroupInfo = QStandardItemModel(self)
-        self.modelParameters = QStandardItemModel(self)
+        self.model_group = QStandardItemModel(self)
+        self.model_group_info = QStandardItemModel(self)
+        self.model_parameters = QStandardItemModel(self)
+        self.model_proxy_parameters = QSortFilterProxyModel(self)
         self.initView()
+        self.createConnection()
+        pass
+
+    def createConnection(self):
+        self.viewGroup.clicked.connect(self.onViewGroupClicked)  
+        
+        pass
+        
+    @pyqtSlot(QModelIndex)
+    def onViewGroupClicked(self, index):
+        row = index.row()
+        column = index.column()
+        grp_name = self.model_group.item(row, 0 ).text() 
+        grp_name = grp_name.split('_')[1]
+        regx = QRegExp(grp_name.strip()) 
+        self.model_proxy_parameters.setFilterKeyColumn(0)
+        self.model_proxy_parameters.setFilterRegExp(regx)
         pass
 
     def readDataFromFile(self):
@@ -28,9 +47,9 @@ class MainWindow(QMainWindow, mainwindow_ui.Ui_MainWindow):
                         contents = f.read()
                     if(filename.lower() == rd.KPD_PARA_TABLE_SRC_FILE ):
                         for item in rd.read_para_table(contents):
-                            self.addRowToModel(self.modelParameters, item)
+                            self.addRowToModel(self.model_parameters, item)
                         for item in rd.read_grp_info(contents):
-                            self.addRowToModel(self.modelGroup, item)
+                            self.addRowToModel(self.model_group, item)
                             pass
                         pass
                     elif( filename.lower() == rd.KPD_ADD_TITLE_SRC_FILE):
@@ -51,14 +70,15 @@ class MainWindow(QMainWindow, mainwindow_ui.Ui_MainWindow):
 
     def initView(self):
         viewList = [self.viewGroup, self.viewGroupInfo, self.viewParameter]
-        self.viewGroup.setModel(self.modelGroup)
-        self.viewGroupInfo.setModel(self.modelGroupInfo)
-        self.viewParameter.setModel(self.modelParameters) 
+        self.viewGroup.setModel(self.model_group)
+        self.viewGroupInfo.setModel(self.model_group_info)
+        self.model_proxy_parameters.setSourceModel(self.model_parameters)
+        self.viewParameter.setModel(self.model_proxy_parameters) 
         
         # row 를 구분하기 위해서 번갈아 가면서 음영을 넣도록 함 
         for item in viewList:
             item.setAlternatingRowColors(True)
-
+            item.setSelectionBehavior(QAbstractItemView.SelectRows)
         pass
 
 if __name__ == '__main__': 
